91a85d9164e79b4a387a4b4a5a15e2a26cff65b3
juliangieseke <j@juliangieseke.de>
2016-11-03 17:30:43 +0100

    Update WinTimeBackup.bat

--- WinTimeBackup.bat
+++ WinTimeBackup.bat
@@ -1,15 +1,13 @@
 @echo off
-
 :: ========================================================================= ::
-:: WinTimeBackup - Time Machine for Windows					     v 0.1 alpha ::
-:: by Julian Gieseke (mail@juliangieseke.de)								 ::
-:: based on ln.exe by Hermann Schinagel (Hermann.Schinagl@gmx.net)			 ::
-:: 																			 ::
-:: This Script is provided AS IS!											 ::
-:: license from ln.exe applies also to this script.							 ::
+:: WinTimeBackup - Time Machine for Windows                                  ::
+:: by Julian Gieseke (mail@juliangieseke.de)                                 ::
+:: based on ln.exe by Hermann Schinagel (Hermann.Schinagl@gmx.net)           ::
+::                                                                           ::
+:: This Script is provided AS IS!                                            ::
 :: ========================================================================= ::
-:: About this Script:														 ::
-::																			 ::
+:: About this Script:                                                        ::
+::                                                                           ::
 :: This script provides a simple implementation of a File History Snapshot   ::
 :: like Backup using ln.exe. Its functionality is highly inspired by Apple   ::
 :: Time Machine and Synology Time Backup/Hyper Backup. Like Time Backup and  ::
@@ -18,7 +16,7 @@
 :: ========================================================================= ::
 :: Changelog:
 ::
-:: v 0.1 alpha (first working version)
+:: v 0.1 alpha 1 (first working version)
 :: - basic time machine like functionality provided
 :: ========================================================================= ::
 
@@ -33,7 +31,7 @@
 
 :: Set Output to cmd (uses echo)
 :: 0 = nothing, 1 = some status, 2 = technical stuff
-set DEBUG=0
+set DEBUG=2
 
 :: ln.exe (change if yours isnt in PATH)
 set LN=ln.exe
@@ -47,18 +45,18 @@ set SRC=D:
 
 :: Destination Folder
 :: you can use %~dp0 if your script is at destionation folder
-::set DST=%~dp0
+set DST=%~dp0
 :: or parameter
 ::set DST=%~1
 :: or something fixed
-set DST=E:\Backups
+::set DST=E:\Backups
 
 :: Name of the Backup, used for subfolder if USESUB=1
-set BKPNAME=TimeBackup
+::set BKPNAME=Backup
 :: if you want you can use a paramter for this
 ::set BKPNAME=%~1
 :: or simply the filename of this script
-::set BKPNAME=%~n0
+set BKPNAME=%~n0
 
 :: Puts all Backups and Logs into a Subfolder named BKPNAME.
 :: set to 1 if you want to use DST/BKPNAME as root folder for Backup 
@@ -90,13 +88,15 @@ set DELETELOGFILES=1
 :: ========================================================================= ::
 
 :: set names of backup sets
-:: they have to be different!
+:: they have to be different and 
+:: you have to restart your backup after changing them! 
 set NAMEFIRST=daily
 set NAMESECOND=weekly
 set NAMETHIRD=monthly
 set NAMEFOURTH=yearly
 
 :: set how many copies of each set should kept
+:: you can change this at any time
 set KEEPFIRST=14
 set KEEPSECOND=8
 set KEEPTHIRD=12
@@ -104,7 +104,7 @@ set KEEPFOURTH=2
 
 
 :: set interval for second, third & fourth 
-:: this is used for ID MOD INTERVAL calulations when moving backups to the next set.
+:: this is used for ID modulo INTERVAL calulations when moving backups to the next set.
 :: !!! These Values have to be SMALLER then their corresponding KEEP*
 :: every week, set to 14 for bi-weekly
 set INTERVALSECOND=7
@@ -116,7 +116,20 @@ set INTERVALFOURTH=12
 :: ========================================================================= ::
 :: INCLUDES/EXCLUDES
 :: ========================================================================= ::
+:: !!! All options also apply when moving backups to the next set !!!
 
+set OPT=%OPT% --excludedir "Archiv"
+set OPT=%OPT% --excludedir "Aufnahmen"
+set OPT=%OPT% --excludedir "Bilder"
+set OPT=%OPT% --excludedir "Dokumente"
+::set OPT=%OPT% --excludedir "Filme"
+set OPT=%OPT% --excludedir "Hörbücher"
+::set OPT=%OPT% --excludedir "Kamera Upload"
+set OPT=%OPT% --excludedir "Lightroom"
+::set OPT=%OPT% --excludedir "Musik"
+::set OPT=%OPT% --excludedir "Resilio"
+::set OPT=%OPT% --excludedir "Serien"
+set OPT=%OPT% --excludedir "SteamApps"
 
 set OPT=%OPT% --excludedir "$RECYCLE.BIN"
 set OPT=%OPT% --excludedir "System Volume Information"
@@ -126,14 +139,18 @@ set OPT=%OPT% --exclude "Thumbs.db"
 set OPT=%OPT% --excludedir ".sync"
 set OPT=%OPT% --excludedir ".git"
 set OPT=%OPT% --excludedir ".svn"
+::set OPT=%OPT% --exclude "*.ts"
 
 
 :: ========================================================================= ::
 :: THATS IT - DONE CHANGE ANYTHING BELOW THIS LINE
 :: ========================================================================= ::
 
+
+set WTB_VERSION=v 0.1 alpha
+
 if %DEBUG% GTR 0 echo =========================================================================
-if %DEBUG% GTR 0 echo WinTimeBackup 0.1 alpha
+if %DEBUG% GTR 0 echo WinTimeBackup %WTB_VERSION%
 
 
 setLocal EnableDelayedExpansion
@@ -143,6 +160,7 @@ for /f "delims=" %%a in ('%LN% --datetime') do set DATETIME=%%a
 
 :: set suffix for copying process.
 set PROGRESSSUFFIX=inprogress
+set ERRORSUFFIX=failed
 
 :: cut off trailing slashes from SRC & DST
 IF %SRC:~-1%==\ SET SRC=%SRC:~0,-1%
@@ -177,6 +195,7 @@ if %USESUB%==1 (
 if not exist "%DST%\%LOGFILEPATH%" mkdir "%DST%\%LOGFILEPATH%"
 set OPT=%OPT% --quiet %LOGLEVEL%
 
+set errlev=0
 
 if %DEBUG% GTR 0 set OPT=--progress %OPT%
 
@@ -193,9 +212,18 @@ if %DEBUG% GTR 0 echo Backing up: "%BKPNAME%"
 if %DEBUG% GTR 0 echo from: "%SRC%"
 if %DEBUG% GTR 0 echo to: "%DST%"
 
+:: @TODO Need to catch CTRL+c somehow to rename failed backups (see below), otherwise after one 
+:: failed no more backups are started...
+:: http://stackoverflow.com/questions/27130050/batch-script-if-user-press-ctrlc-do-a-command-before-exitting
+if exist "????-??-?? ??-??-?? * %PROGRESSSUFFIX%" (
+	if %DEBUG% GTR 0 echo =========================================================================
+	if %DEBUG% GTR 0 echo Already Backing up, skipping this run!
+	
+	goto errorexit
+)
 
 if %DEBUG% GTR 0 echo =========================================================================
-set errlev=0
+
 if exist "????-??-?? ??-??-?? * %NAMEFIRST%" (
 	:: get last backup and increase its ID by 1
 	for /f "tokens=1,2,3 delims= " %%a in ('dir /b /AD /O:N "????-??-?? ??-??-?? * %NAMEFIRST%"') do ( 
@@ -212,9 +240,10 @@ if exist "????-??-?? ??-??-?? * %NAMEFIRST%" (
 	if %DEBUG% GTR 1 echo Calling ln.exe:
 	if %DEBUG% GTR 1 echo.
 	if %DEBUG% GTR 1 echo %LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.copy.log" --delorean "%SRC%" "%LNDEST%\!LastBackup!" "%LNDEST%\%DATETIME% !NextId! %NAMEFIRST% %PROGRESSSUFFIX%"
+	
 	if %DEBUG% GTR 0 echo =========================================================================
 	if %DEBUG% GTR 0 echo ln.exe progress:
-
+	
 	%LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.copy.log" --delorean "%SRC%" "%LNDEST%\!LastBackup!" "%LNDEST%\%DATETIME% !NextId! %NAMEFIRST% %PROGRESSSUFFIX%"
 	
 	set errlev=!errorlevel!
@@ -233,7 +262,7 @@ if exist "????-??-?? ??-??-?? * %NAMEFIRST%" (
 	if %DEBUG% GTR 1 echo %LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.copy.log" --copy "%SRC%" "%LNDEST%\%DATETIME% !NextId! %NAMEFIRST% %PROGRESSSUFFIX%"
 	if %DEBUG% GTR 0 echo =========================================================================
 	if %DEBUG% GTR 0 echo ln.exe progress:
-
+	
 	%LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.copy.log" --copy "%SRC%" "%LNDEST%\%DATETIME% !NextId! %NAMEFIRST% %PROGRESSSUFFIX%"
 	
 	set errlev=!errorlevel!
@@ -244,29 +273,58 @@ if %DEBUG% GTR 1 echo ln.exe Returncode was "%errlev%" and its output was:
 if %DEBUG% GTR 1 echo.
 if %DEBUG% GTR 1 type "%LOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.copy.log"
 
-if !errlev! NEQ 0 goto errorexit
+if !errlev! NEQ 0 ( 
+	if %DEBUG% GTR 0 echo =========================================================================
+	if %DEBUG% GTR 0 echo Backup failed. Trying to rename failed backup:
+	
+	if %DEBUG% GTR 1 echo =========================================================================
+	if %DEBUG% GTR 1 echo Calling ln.exe:
+	if %DEBUG% GTR 1 echo.
+	if %DEBUG% GTR 1 echo %LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.errorlog" --move "%LNDEST%\%DATETIME% !NextId! %NAMEFIRST% %PROGRESSSUFFIX%" "%LNDEST%\%DATETIME% !NextId! %NAMEFIRST% %ERRORSUFFIX%"
+	if %DEBUG% GTR 0 echo =========================================================================
+	if %DEBUG% GTR 0 echo ln.exe progress:
+	
+	%LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.errorlog" --move "%LNDEST%\%DATETIME% !NextId! %NAMEFIRST% %PROGRESSSUFFIX%" "%LNDEST%\%DATETIME% !NextId! %NAMEFIRST% %ERRORSUFFIX%"
+	
+	set errlev2=!errorlevel!
+	
+	if %DEBUG% GTR 1 echo =========================================================================
+	if %DEBUG% GTR 1 echo ln.exe Returncode was "%errlev%" and its output was:
+	if %DEBUG% GTR 1 echo.
+	if %DEBUG% GTR 1 type "%LOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.errorlog"
+	
+	if errlev2 EQU 0 (
+		if %DEBUG% GTR 0 echo =========================================================================
+		if %DEBUG% GTR 0 echo Renaming successfull
+	)
+	
+	::prev error..
+	goto errorexit
+) else (
+	if %DEBUG% GTR 0 echo =========================================================================
+	if %DEBUG% GTR 0 echo Copying successfull. Renaming folder to "%DATETIME% %NextId% %NAMEFIRST%"
+	
+	if %DEBUG% GTR 1 echo =========================================================================
+	if %DEBUG% GTR 1 echo Calling ln.exe:
+	if %DEBUG% GTR 1 echo.
+	if %DEBUG% GTR 1 echo %LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.move.log" --move "%LNDEST%\%DATETIME% %NextId% %NAMEFIRST% %PROGRESSSUFFIX%" "%LNDEST%\%DATETIME% %NextId% %NAMEFIRST%"
+	if %DEBUG% GTR 0 echo =========================================================================
+	if %DEBUG% GTR 0 echo ln.exe progress:
+	
+	%LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.move.log" --move "%LNDEST%\%DATETIME% %NextId% %NAMEFIRST% %PROGRESSSUFFIX%" "%LNDEST%\%DATETIME% %NextId% %NAMEFIRST%"
 
-:: renaming folder if everything is fine
-if %DEBUG% GTR 0 echo =========================================================================
-if %DEBUG% GTR 0 echo Copying successfull.
+	set errlev=!errorlevel!
 
-if %DEBUG% GTR 0 echo =========================================================================
-if %DEBUG% GTR 0 echo Renaming folder to "%DATETIME% %NextId% %NAMEFIRST%"
+	if %DEBUG% GTR 1 echo =========================================================================
+	if %DEBUG% GTR 1 echo ln.exe Returncode was "%errlev%" and its output was:
+	if %DEBUG% GTR 1 echo.
+	if %DEBUG% GTR 1 type "%LOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.move.log"
 
-%LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.move.log" --quiet %LOGLEVEL% --move "%LNDEST%\%DATETIME% %NextId% %NAMEFIRST% %PROGRESSSUFFIX%" "%LNDEST%\%DATETIME% %NextId% %NAMEFIRST%"
+	if !errlev! NEQ 0 goto errorexit
 
-set errlev=!errorlevel!
-
-if %DEBUG% GTR 1 echo =========================================================================
-if %DEBUG% GTR 1 echo ln.exe Returncode was "%errlev%" and its output was:
-if %DEBUG% GTR 1 echo.
-if %DEBUG% GTR 1 type "%LOGFILEPATH%\%DATETIME% !NextId! %NAMEFIRST%.move.log"
-
-if !errlev! NEQ 0 goto errorexit
-
-if %DEBUG% GTR 0 echo =========================================================================
-if %DEBUG% GTR 0 echo Renaming successfull
-
+	if %DEBUG% GTR 0 echo =========================================================================
+	if %DEBUG% GTR 0 echo Renaming successfull
+)
 
 :: ========================================================================= ::
 :: PART II: Checking/Renaming old Backups
@@ -290,17 +348,15 @@ for /f "skip=%KEEPFIRST% tokens=1,2,3 delims= " %%a in ('dir /b /A:D /O:-N "????
 	if !MODULO! NEQ 0 (
 		if %DEBUG% GTR 0 echo Removing old backup set "%%a %%b %%c %NAMEFIRST%" ...
 		%LN% --deeppathdelete "%%a %%b %%c %NAMEFIRST%" > nul
+		
+		if %DELETELOGFILES%==1 (
+			if %DEBUG% GTR 0 echo Removing logfiles for "%%a %%b %%c %NAMEFIRST%" ...
+			del "%LOGFILEPATH%\%%a %%b %%c *.log" > nul
+		)
 	) else (
 		if %DEBUG% GTR 0 echo Renaming old backup set "%%a %%b %%c %NAMEFIRST%"
 		if %DEBUG% GTR 0 echo                      to "%%a %%b %%c %NAMESECOND%" ...
 		%LN% --output "%RLOGFILEPATH%\%%a %%b %%c %NAMESECOND%.move.log" --move "%LNDEST%\%%a %%b %%c %NAMEFIRST%" "%LNDEST%\%%a %%b %%c %NAMESECOND%" 
-	)
-	
-	if %DELETELOGFILES%==1 (
-		if %DEBUG% GTR 0 echo Removing logfile "%%a %%b %%c %NAMEFIRST%.copy.log" ...
-		del "%LOGFILEPATH%\%%a %%b %%c %NAMEFIRST%.copy.log" > nul
-		if %DEBUG% GTR 0 echo Removing logfile "%%a %%b %%c %NAMEFIRST%.move.log" ...
-		del "%LOGFILEPATH%\%%a %%b %%c %NAMEFIRST%.move.log" > nul
 	)
 )
 
@@ -316,15 +372,15 @@ if exist "????-??-?? ??-??-?? * %NAMESECOND%" (
 		if !MODULO! NEQ 0 (
 			if %DEBUG% GTR 0 echo Removing old backup set "%%a %%b %%c %NAMESECOND%" ...
 			%LN% --deeppathdelete "%%a %%b %%c %NAMESECOND%" > nul
+			
+			if %DELETELOGFILES%==1 (
+				if %DEBUG% GTR 0 echo Removing logfiles for "%%a %%b %%c %NAMESECOND%" ...
+				del "%LOGFILEPATH%\%%a %%b %%c *.log" > nul
+			)
 		) else (
 			if %DEBUG% GTR 0 echo Renaming old backup set "%%a %%b %%c %NAMESECOND%"
 			if %DEBUG% GTR 0 echo                      to "%%a %%b %%c %NAMETHIRD%" ...
 			%LN% --output "%RLOGFILEPATH%\%%a %%b %%c %NAMETHIRD%.move.log" --move "%LNDEST%\%%a %%b %%c %NAMESECOND%" "%LNDEST%\%%a %%b %%c %NAMETHIRD%" 
-		)
-		
-		if %DELETELOGFILES%==1 (
-			if %DEBUG% GTR 0 echo Removing logfile "%%a %%b %%c %NAMESECOND%.move.log" ...
-			del "%LOGFILEPATH%\%%a %%b %%c %NAMESECOND%.move.log" > nul
 		)
 	)
 )
@@ -341,15 +397,15 @@ if exist "????-??-?? ??-??-?? * %NAMETHIRD%" (
 		if !MODULO! NEQ 0 (
 			if %DEBUG% GTR 0 echo Removing old backup set "%%a %%b %%c %NAMETHIRD%" ...
 			%LN% --deeppathdelete "%%a %%b %%c %NAMETHIRD%" > nul
+
+			if %DELETELOGFILES%==1 (
+				if %DEBUG% GTR 0 echo Removing logfiles for "%%a %%b %%c %NAMETHIRD%" ...
+				del "%LOGFILEPATH%\%%a %%b %%c *.log" > nul
+			)
 		) else (
 			if %DEBUG% GTR 0 echo Renaming old backup set "%%a %%b %%c %NAMETHIRD%" 
 			if %DEBUG% GTR 0 echo                      to "%%a %%b %%c %NAMEFOURTH%" ...
 			%LN% --output "%RLOGFILEPATH%\%%a %%b %%c %NAMEFOURTH%.move.log" --move "%LNDEST%\%%a %%b %%c %NAMETHIRD%" "%LNDEST%\%%a %%b %%c %NAMEFOURTH%" 
-		)
-		
-		if %DELETELOGFILES%==1 (
-			if %DEBUG% GTR 0 echo Removing logfile "%%a %%b %%c %NAMETHIRD%.move.log" ...
-			del "%LOGFILEPATH%\%%a %%b %%c %NAMETHIRD%.move.log" > nul
 		)
 	)
 )
@@ -363,20 +419,20 @@ if exist "????-??-?? ??-??-?? * %NAMEFOURTH%" (
 		%LN% --deeppathdelete "%%a %%b %%c %NAMEFOURTH%" > nul
 		
 		if %DELETELOGFILES%==1 (
-			if %DEBUG% GTR 0 echo Removing logfile "%%a %%b %%c %NAMEFOURTH%.move.log" ...
-			del "%LOGFILEPATH%\%%a %%b %%c %NAMEFOURTH%.move.log" > nul
+			if %DEBUG% GTR 0 echo Removing logfiles for "%%a %%b %%c %NAMEFOURTH%" ...
+			del "%LOGFILEPATH%\%%a %%b %%c *.log" > nul
 		)
 	)
 )
 
-
 popd
-
 
 if %DEBUG% GTR 0 echo =========================================================================
 if %DEBUG% GTR 0 echo Done.
+if %DEBUG% GTR 0 echo =========================================================================
 
 :errorexit
+if %DEBUG% GTR 0 echo =========================================================================
 exit /b %errlev%
 
 
