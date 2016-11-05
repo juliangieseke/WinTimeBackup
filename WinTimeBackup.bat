983f707213027cd91939a0e55df78f952c370be6
Julian Gieseke <github@juliangieseke.de>
2016-11-05 18:43:27 +0100

    changed path handling, refactoring, wip backup handling

--- WinTimeBackup.bat
+++ WinTimeBackup.bat
@@ -61,7 +61,7 @@ set BKPNAME=%~n0
 
 :: Puts all Backups and Logs into a Subfolder named BKPNAME.
 :: set to 1 if you want to use DST/BKPNAME as root folder for Backup 
-set USESUB=0
+set USESUB=1
 
 :: Set to 1,2,3 if you want to save ln.exes output to logfiles 
 :: for more info on loglevel see ln.exe --quiet doc 
@@ -99,34 +99,39 @@ set NAMEFOURTH=Y
 
 :: set how many copies of each set should kept
 :: you can change this at any time, but keep them >0!
-set KEEPFIRST=28
-set KEEPSECOND=8
-set KEEPTHIRD=9
-set KEEPFOURTH=1
+::set KEEPFIRST=28
+::set KEEPSECOND=8
+::set KEEPTHIRD=9
+::set KEEPFOURTH=1
 :: @TODO: allow 0
+set KEEPFIRST=4
+set KEEPSECOND=3
+set KEEPTHIRD=2
+set KEEPFOURTH=1
+
 
 
 :: set interval for second, third & fourth 
 :: this is used for ID modulo INTERVAL calulations when moving backups to the next set.
 :: !!! These Values have to be SMALLER OR EQUAL to the KEEP* BEFORE it (INTERVALTHIRD <= KEEPSECOND )
 :: every week, set to 14 for bi-weekly
-set INTERVALSECOND=7
+set INTERVALSECOND=2
 :: every four weeks
-set INTERVALTHIRD=4
+set INTERVALTHIRD=2
 :: every 12 months
-set INTERVALFOURTH=12
+set INTERVALFOURTH=2
 
 :: ========================================================================= ::
 :: INCLUDES/EXCLUDES
 :: ========================================================================= ::
 :: !!! All options also apply when moving backups to the next set !!!
 
-set OPT=%OPT% --excludedir "Archiv"
+set OPT=--excludedir "Archiv"
 set OPT=%OPT% --excludedir "Aufnahmen"
 set OPT=%OPT% --excludedir "Bilder"
 set OPT=%OPT% --excludedir "Dokumente"
 ::set OPT=%OPT% --excludedir "Filme"
-set OPT=%OPT% --excludedir "HÃ¶rbÃ¼cher"
+set OPT=%OPT% --excludedir "H”rbcher"
 set OPT=%OPT% --excludedir "Kamera Upload"
 set OPT=%OPT% --excludedir "Lightroom"
 set OPT=%OPT% --excludedir "Musik"
@@ -185,49 +190,39 @@ set DATEFORMAT=????????-??????
 :: set suffix for copying process.
 set COPYSUFFIX=wip
 set ERRORSUFFIX=failed
+set DOUBLESUFFIX=duplicate
 
-:: cut off trailing slashes from SRC & DST
-IF %SRC:~-1%==\ SET SRC=%SRC:~0,-1%
-IF %DST:~-1%==\ SET DST=%DST:~0,-1%
+:: cut off trailing slashes from user inputs
+IF %SRC:~-1% EQU \ SET SRC=%SRC:~0,-1%
+IF %DST:~-1% EQU \ SET DST=%DST:~0,-1%
+IF %LOGFILEPATH:~-1% EQU \ SET LOGFILEPATH=%LOGFILEPATH:~0,-1%
 
-:: get GUID from drive letter for SRC & DST
-set SRCLETTER=%SRC:~0,1%
-set SRCPATH=%SRC:~3%
-for /f "delims= " %%a in ('mountvol %SRCLETTER%:\ /L') do set SRCVOL=%%a
-set SRCVOL=%SRCVOL:~0,-1%
+:: get GUID from drive letter for DST 
+:: and save it for ln specific path
+for /f "delims= " %%a in ('mountvol %DST:~0,2%\ /L') do set DSTVOL=%%a
 
-set DSTLETTER=%DST:~0,1%
-set DSTPATH=%DST:~3%
-for /f "delims= " %%a in ('mountvol %DSTLETTER%:\ /L') do set DSTVOL=%%a
-set DSTVOL=%DSTVOL:~0,-1%
-
-:: ln.exe should use GUID for Destination
-set LNDEST=%DSTVOL%\%DSTPATH%
-
-:: and needs a relative logfilepath
-set RLOGFILEPATH=%LOGFILEPATH%
+:: ln.exe should use GUID instead of Drive Letter
+set LNDST=%DSTVOL:~0,-1%\%DST:~3%
 
 :: add %BKPNAME% to dest if USESUB==1
-if %USESUB%==1 (
-	set LNDEST=%LNDEST%\%BKPNAME%
+if %USESUB% EQU 1 (
+	set LNDST=%LNDST%\%BKPNAME%
 	set DST=%DST%\%BKPNAME%
-	set DSTPATH=%DSTPATH%\%BKPNAME%
-	set RLOGFILEPATH=%BKPNAME%\%LOGFILEPATH%
-	if not exist "!DST!" mkdir "!DST!"
+	set LOGFILEPATH=%LOGFILEPATH%
+	if not exist "%BKPNAME%" mkdir "%BKPNAME%"
 )
 
-:: add logfile & loglevel
-if not exist "%DST%\%LOGFILEPATH%" mkdir "%DST%\%LOGFILEPATH%"
-set OPT=%OPT% --quiet %LOGLEVEL%
+:: store DST as CD
+pushd %DST%
 
-set errlev=0
+:: add loglevel & loglevel
+if not exist "%LOGFILEPATH%" mkdir "%LOGFILEPATH%"
+set OPT=%OPT% --quiet %LOGLEVEL%
 
 :: ========================================================================= ::
 :: PART I: Copying new Files
 :: ========================================================================= ::
 
-
-pushd %DST%
 
 if %DEBUG% GTR 0 echo.
 if %DEBUG% GTR 0 echo Backup Name: "%BKPNAME%" 
@@ -237,12 +232,14 @@ if %DEBUG% GTR 0 echo          To: "%DST%"
 :: @TODO Need to catch CTRL+c somehow to rename failed backups (see below), otherwise after one 
 :: failed no more backups are started...
 :: http://stackoverflow.com/questions/27130050/batch-script-if-user-press-ctrlc-do-a-command-before-exitting
-if exist "%DATEFORMAT%%DLMTR%*%DLMTR%%COPYSUFFIX%" (
-	if %DEBUG% GTR 0 echo. && echo ========================================================================= && echo.
-	if %DEBUG% GTR 0 echo Already Backing up, skipping this run!
-	
-	goto errorexit
-)
+::if exist "%DATEFORMAT%%DLMTR%*%DLMTR%%COPYSUFFIX%" (
+::	if %DEBUG% GTR 0 echo. && echo ========================================================================= && echo.
+::	if %DEBUG% GTR 0 echo Already Backing up, skipping this run!
+::	
+::	goto errorexit
+::)
+:: maybe better: check at the end if finished backup with same id exists. (see below)
+
 
 if %DEBUG% GTR 0 echo. && echo ========================================================================= && echo.
 
@@ -252,39 +249,50 @@ if exist "%DATEFORMAT%%DLMTR%*%DLMTR%%NAMEFIRST%" (
 		set "LastBackup=%%a%DLMTR%%%b%DLMTR%%NAMEFIRST%"
 		set /a NextId=%%b+1
 	)
-	popd
 	
 	if %DEBUG% GTR 0 echo Found old backup: "!LastBackup!"
 
-	set LNPARAMS=--output "%RLOGFILEPATH%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%.copy.log" --delorean "%SRC%" "%LNDEST%\!LastBackup!" "%LNDEST%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%"
+	set LNPARAMS=--output "%LOGFILEPATH%\%DATETIME%%DLMTR%!NextId!.copy.log" --delorean "%SRC%" "%LNDST%\!LastBackup!" "%LNDST%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%"
 	
 ) else (
 	:: first backup! copying the files
-	popd
 	set NextId=0
 	
 	if %DEBUG% GTR 0 echo No old backup found.
 	
-	set LNPARAMS=--output "%RLOGFILEPATH%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%.copy.log" --copy "%SRC%" "%LNDEST%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%"
+	set LNPARAMS=--output "%LOGFILEPATH%\%DATETIME%%DLMTR%!NextId!.copy.log" --copy "%SRC%" "%LNDST%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%"
 )
 
-if %DEBUG% GTR 0 echo.
-if %DEBUG% GTR 0 echo Copying to "%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%"...
+if %DEBUG% GTR 0 echo. && echo ========================================================================= && echo.
+if %DEBUG% GTR 0 echo Copying to "%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%"...
 
 
 if %DEBUG% GTR 0 echo.
 if %DEBUG% GTR 0 echo %LN% %OPT% %LNPARAMS%
 
+:: calling ln.exe here
 %LN% %OPT% %LNPARAMS%
 
 if %DEBUG% GTR 0 echo.
-if %DEBUG% GTR 0 echo %LN% exit code was %errlev%
+if %DEBUG% GTR 0 echo %LN% exit code was  %errorlevel%
 	
-if %errlev% NEQ 0 ( 
+if %errorlevel% NEQ 0 ( 
 	if %DEBUG% GTR 0 echo.
-	if %DEBUG% GTR 0 echo Copying failed! See Logs for details
+	if %DEBUG% GTR 0 echo Copying failed. See Logfile "%LOGFILEPATH%\%DATETIME%%DLMTR%%NextId%.copy.log" for details
 	
-	%LN% %OPT% --output "%RLOGFILEPATH%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%.move.errlog" --move "%LNDEST%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%" "%LNDEST%\%DATETIME%%DLMTR%!NextId!%DLMTR%%NAMEFIRST%%DLMTR%%ERRORSUFFIX%"
+	%LN% --quiet --move "%LNDST%\%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%" "%LNDST%\%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%%DLMTR%%ERRORSUFFIX%"
+	
+	goto errorexit
+)
+
+:: what happens if second backup is faster then first?
+:: what does that mean for data consistency?
+:: @TODO: test it!!
+if exist "%DATEFORMAT%%DLMTR%%NextID%" (
+	if %DEBUG% GTR 0 echo. && echo ========================================================================= && echo.
+	if %DEBUG% GTR 0 echo Backup with same ID already exists, marking this one as duplicate.
+	
+	%LN% --quiet --move "%LNDST%\%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%" "%LNDST%\%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%%DLMTR%%DOUBLESUFFIX%"
 	
 	goto errorexit
 )
@@ -300,15 +308,15 @@ if %DEBUG% GTR 0 echo Copying successful.
 :: first set intervals to needed values.
 set /a INTERVALTHIRD=%INTERVALTHIRD%*%INTERVALSECOND%
 set /a INTERVALFOURTH=%INTERVALFOURTH%*%INTERVALTHIRD% 
-:: decrease KEEPFIRST by 1 because the newest backup still has %PROCESSSUFFIX% and isnt counted
+
+:: decrease KEEPFIRST by 1 because the newest backup still 
+:: has %PROCESSSUFFIX% and isnt counted and skip=0 wont work...
 if %KEEPFIRST% GTR 1 set /a KEEPFIRST=%KEEPFIRST%-1
 
-pushd %DST%
 if exist "%DATEFORMAT%%DLMTR%*%DLMTR%%NAMEFIRST%" (
 	if %DEBUG% GTR 0 echo. && echo ========================================================================= && echo.
 	if %DEBUG% GTR 0 echo Checking old %NAMEFIRST% backups ...
 
-	
 	for /f "skip=%KEEPFIRST% tokens=1,2 delims=%DLMTR%" %%a in ('dir /b /A:D /O:-N "%DATEFORMAT%%DLMTR%*%DLMTR%%NAMEFIRST%"') do (
 		set /a MODULO=%%b %% %INTERVALSECOND%
 		
@@ -320,14 +328,14 @@ if exist "%DATEFORMAT%%DLMTR%*%DLMTR%%NAMEFIRST%" (
 			%LN% --deeppathdelete "%%a%DLMTR%%%b%DLMTR%%NAMEFIRST%" > nul
 			if %DELETELOGFILES%==1 (
 				if %DEBUG% GTR 0 echo Removing logfiles for "%%a%DLMTR%%%b%DLMTR%%NAMEFIRST%" ...
-				del "%LOGFILEPATH%\%%a%DLMTR%%%b%DLMTR%*.log" > nul
+				del "%LOGFILEPATH%\%%a%DLMTR%%%b*.log" > nul
 			)
 			
 		) else (
 			if %DEBUG% GTR 0 echo Renaming old backup set "%%a%DLMTR%%%b%DLMTR%%NAMEFIRST%"
 			if %DEBUG% GTR 0 echo                      to "%%a%DLMTR%%%b%DLMTR%%NAMESECOND%" ...
 			
-			%LN% --quiet --move "%LNDEST%\%%a%DLMTR%%%b%DLMTR%%NAMEFIRST%" "%LNDEST%\%%a%DLMTR%%%b%DLMTR%%NAMESECOND%" > nul
+			%LN% --quiet --move "%LNDST%\%%a%DLMTR%%%b%DLMTR%%NAMEFIRST%" "%LNDST%\%%a%DLMTR%%%b%DLMTR%%NAMESECOND%" > nul
 		)
 	)
 )
@@ -348,12 +356,12 @@ if exist "%DATEFORMAT%%DLMTR%*%DLMTR%%NAMESECOND%" (
 			
 			if %DELETELOGFILES%==1 (
 				if %DEBUG% GTR 0 echo Removing logfiles for "%%a%DLMTR%%%b%DLMTR%%NAMESECOND%" ...
-				del "%LOGFILEPATH%\%%a%DLMTR%%%b%DLMTR%*.log" > nul
+				del "%LOGFILEPATH%\%%a%DLMTR%%%b*.log" > nul
 			)
 		) else (
 			if %DEBUG% GTR 0 echo Renaming old backup set "%%a%DLMTR%%%b%DLMTR%%NAMESECOND%"
 			if %DEBUG% GTR 0 echo                      to "%%a%DLMTR%%%b%DLMTR%%NAMETHIRD%" ...
-			%LN% --quiet --move "%LNDEST%\%%a%DLMTR%%%b%DLMTR%%NAMESECOND%" "%LNDEST%\%%a%DLMTR%%%b%DLMTR%%NAMETHIRD%" > nul
+			%LN% --quiet --move "%LNDST%\%%a%DLMTR%%%b%DLMTR%%NAMESECOND%" "%LNDST%\%%a%DLMTR%%%b%DLMTR%%NAMETHIRD%" > nul
 		)
 	)
 )
@@ -374,12 +382,12 @@ if exist "%DATEFORMAT%%DLMTR%*%DLMTR%%NAMETHIRD%" (
 
 			if %DELETELOGFILES%==1 (
 				if %DEBUG% GTR 0 echo Removing logfiles for "%%a%DLMTR%%%b%DLMTR%%NAMETHIRD%" ...
-				del "%LOGFILEPATH%\%%a%DLMTR%%%b%DLMTR%*.log" > nul
+				del "%LOGFILEPATH%\%%a%DLMTR%%%b*.log" > nul
 			)
 		) else (
 			if %DEBUG% GTR 0 echo Renaming old backup set "%%a%DLMTR%%%b%DLMTR%%NAMETHIRD%" 
 			if %DEBUG% GTR 0 echo                      to "%%a%DLMTR%%%b%DLMTR%%NAMEFOURTH%" ...
-			%LN% --quiet --move "%LNDEST%\%%a%DLMTR%%%b%DLMTR%%NAMETHIRD%" "%LNDEST%\%%a%DLMTR%%%b%DLMTR%%NAMEFOURTH%" > nul
+			%LN% --quiet --move "%LNDST%\%%a%DLMTR%%%b%DLMTR%%NAMETHIRD%" "%LNDST%\%%a%DLMTR%%%b%DLMTR%%NAMEFOURTH%" > nul
 		)
 	)
 )
@@ -395,12 +403,10 @@ if exist "%DATEFORMAT%%DLMTR%*%DLMTR%%NAMEFOURTH%" (
 		
 		if %DELETELOGFILES%==1 (
 			if %DEBUG% GTR 0 echo Removing logfiles for "%%a%DLMTR%%%b%DLMTR%%NAMEFOURTH%" ...
-			del "%LOGFILEPATH%\%%a%DLMTR%%%b%DLMTR%*.log" > nul
+			del "%LOGFILEPATH%\%%a%DLMTR%%%b*.log" > nul
 		)
 	)
 )
-
-popd
 
 :: ========================================================================= ::
 :: PART THREE: Renaming last backup (removing %PROCESSSUFFIX%)
@@ -411,15 +417,16 @@ if %DEBUG% GTR 0 echo. && echo =================================================
 if %DEBUG% GTR 0 echo Renaming folder "%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%"
 if %DEBUG% GTR 0 echo              to "%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%"
 
-%LN% --quiet --move "%LNDEST%\%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%" "%LNDEST%\%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%" > nul
+%LN% --quiet --move "%LNDST%\%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%%DLMTR%%COPYSUFFIX%" "%LNDST%\%DATETIME%%DLMTR%%NextId%%DLMTR%%NAMEFIRST%" > nul
 
 
 :errorexit
 if %DEBUG% GTR 0 echo. && echo ========================================================================= && echo.
 if %DEBUG% GTR 0 echo Done.
 if %DEBUG% GTR 0 echo. && echo ========================================================================= && echo.
-exit /b %errlev%
 
+popd
 
+exit /b %errorlevel%
 
 
